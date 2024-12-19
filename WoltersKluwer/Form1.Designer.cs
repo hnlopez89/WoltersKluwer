fcebbe1db0e949224d2fd3fb89a9f510b14bbc1c
hnlopez89 <hnlopez89@gmail.com>
2024-12-19 13:38:24 +0100

    Agregar archivos de proyecto.

---
+++ WoltersKluwer/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WoltersKluwer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridViewClients = new System.Windows.Forms.DataGridView();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.inputFilePath = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.gridViewClients)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridViewClients
+            // 
+            this.gridViewClients.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridViewClients.Location = new System.Drawing.Point(12, 38);
+            this.gridViewClients.Name = "gridViewClients";
+            this.gridViewClients.Size = new System.Drawing.Size(776, 360);
+            this.gridViewClients.TabIndex = 0;
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(12, 415);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(95, 23);
+            this.btnProcess.TabIndex = 1;
+            this.btnProcess.Text = "Procesar";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(692, 12);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(96, 23);
+            this.btnSelect.TabIndex = 1;
+            this.btnSelect.Text = "Seleccionar";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // inputFilePath
+            // 
+            this.inputFilePath.BackColor = System.Drawing.SystemColors.Control;
+            this.inputFilePath.Location = new System.Drawing.Point(12, 12);
+            this.inputFilePath.Name = "inputFilePath";
+            this.inputFilePath.Size = new System.Drawing.Size(674, 20);
+            this.inputFilePath.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(801, 450);
+            this.Controls.Add(this.inputFilePath);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.gridViewClients);
+            this.Name = "Form1";
+            this.Text = "Importación de Clientes";
+            ((System.ComponentModel.ISupportInitialize)(this.gridViewClients)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gridViewClients;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.TextBox inputFilePath;
+    }
+}
+
---
+++ WoltersKluwer/Form1.cs
@@ -0,0 +1,129 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WoltersKluwer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Archivos Excel (*.xlsx)|*.xlsx";
+                openFileDialog.Title = "Seleccionar archivo Excel";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    inputFilePath.Text = openFileDialog.FileName;
+                    LoadDataFromExcelFile(openFileDialog.FileName);
+                }
+            }
+        }
+
+        private void LoadDataFromExcelFile(string inputFilePath)
+        {
+            try
+            {
+                var dataTable = new DataTable();
+                using (var workbook = new XLWorkbook(inputFilePath))
+                {
+                    var worksheet = workbook.Worksheets.First();
+                    bool firstRow = true;
+                    foreach (var row in worksheet.RowsUsed())
+                    {
+                        if (firstRow)
+                        {
+                            foreach (var cell in row.Cells())
+                            {
+                                dataTable.Columns.Add(cell.Value.ToString());
+                            }
+                            firstRow = false;
+                        }
+                        else
+                        {
+                            dataTable.Rows.Add(row.Cells().Select(c => c.Value.ToString()).ToArray());
+                        }
+                    }
+                }
+                gridViewClients.DataSource = dataTable;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al cargar el archivo Excel: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            if(gridViewClients.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Por favor, seleccione una fila para procesar.", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            var selectedRow = gridViewClients.SelectedRows[0];
+
+
+            if (selectedRow.Cells["Nif"].Value == null ||
+                selectedRow.Cells["Nombre comercial de la organización"].Value == null ||
+                selectedRow.Cells["Fecha alta"].Value == null ||
+                selectedRow.Cells["Código postal"].Value == null ||
+                selectedRow.Cells["Cód. provincia fiscal"].Value == null)
+            {
+                MessageBox.Show("La fila seleccionada contiene datos incompletos. Por favor, revise antes de procesar.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                var client = new Client
+                {
+                    Nif = selectedRow?.Cells["Nif"].Value.ToString(),
+                    Nombre = selectedRow?.Cells["Nombre comercial de la organización"].Value.ToString(),
+                    FechaNacimiento = DateTime.Parse(selectedRow?.Cells["Fecha alta"].Value.ToString()),
+                    CodigoPostal = selectedRow?.Cells["Código postal"].Value.ToString(),
+                    Provincia = selectedRow?.Cells["Cód. provincia fiscal"].Value.ToString(),
+                };
+                InsertarCliente(client);
+
+                MessageBox.Show($"Cliente {client.Nombre} procesado correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al procesar el cliente: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void InsertarCliente(Client cliente)
+        {
+            return;
+        }
+
+
+    }
+
+    public class Client
+    {
+        public string Nif { get; set; }
+        public string Nombre { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string CodigoPostal { get; set; }
+        public string Provincia { get; set; }
+    }
+
+}
